db38be201bf50c7dd6ecce45c26f63129a37f117
alielhasan85 <elhasan.ali@gmail.com>
2025-03-05 13:29:33 +0300

    react crush course

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <>
+      <Post author="alirr " body="react great" />
+      <Post author="naya" body="So smart girl" />
+      <Post> </Post>
+    </>
+  );
+}
+
+export default App;
