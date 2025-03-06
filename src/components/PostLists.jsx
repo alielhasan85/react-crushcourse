36cba3d9d08d6e0c6e43d33bf8d4c5b2dde474f2
alielhasan85 <elhasan.ali@gmail.com>
2025-03-06 09:26:04 +0300

    state in react

--- src/components/PostLists.jsx
+++ src/components/PostLists.jsx
@@ -1,14 +1,18 @@
 import Post from "./Post.jsx";
 import classes from "./PostLists.module.css";
+import NewPost from "./NewPost.jsx";
 
 function PostLists() {
   return (
-    <ul className={classes.posts}>
-      <Post author="John" body="Hello, world!" />
-      <Post author="Jane" body="Hi, there!" />
-      <Post author="alirr " body="react great" />
-      <Post author="naya" body="So smart girl" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="John" body="Hello, world!" />
+        <Post author="Jane" body="Hi, there!" />
+        <Post author="alirr " body="react great" />
+        <Post author="naya" body="So smart girl" />
+      </ul>
+    </>
   );
 }
 
